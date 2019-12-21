097798ee09ce084a7b73c2f95792f865463fb363
zhangKokiJoe <zhangkoki970324@outlook.com>
2019-12-21 18:49:53 +0800

    init repo

---
+++ src/main/java/com/zhang/syliennote/SyliennoteApplication.java
@@ -0,0 +1,13 @@
+package com.zhang.syliennote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyliennoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyliennoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhang/syliennote/SyliennoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.syliennote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyliennoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
